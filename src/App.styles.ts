189584423e5de608eecfe91c9bbe6f605b2ef4af
abdulkhamid <abdulkhamid1828@gmail.com>
2022-03-10 07:18:15 +0500

    add: Question number

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -54,10 +54,21 @@ export const Wrapper = styled.div`
     box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.25);
     border-radius: 10px;
     height: 40px;
-    margin: 20px 0;
+    margin: 20px 5px;
     padding: 0 40px;
   }
   .start {
     max-width: 200px;
+  }
+  .question_number{
+    max-width: 100px;
+    cursor: pointer;
+    background: linear-gradient(180deg, #ffffff, #ffcc91);
+    border: 2px solid #d38558;
+    box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.25);
+    border-radius: 10px;
+    padding: 0 10px;
+    height: 40px;
+    margin: 20px 5px;
   }
 `;
